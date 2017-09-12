565f2ce8efbbf280d444d47deaa93e525b824619
liuqiang <1084618046@qq.com>
2017-09-12 10:25:23 +0800

    test

--- UIAlertControllerCustom/AlertDemo/AlertDemo/ViewController.m
+++ UIAlertControllerCustom/AlertDemo/AlertDemo/ViewController.m
@@ -18,6 +18,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    NSLog(@"asdd");
+    
 //    UIAlertController
     // Do any additional setup after loading the view, typically from a nib.
 }
